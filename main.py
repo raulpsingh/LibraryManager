5bd107add1872677d6bd103978824ea0e7e353fa
raulpsingh <raulpsingh2@gmail.com>
2024-12-11 23:29:26 +0300

    Updated

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ from presentation.cli.menu import display_menu, add_book, delete_book, search_bo
 
 
 def main():
+    # Создание экземпляра репозитория с указанием пути к файлу базы данных
     repo = JsonLibraryRepository("infrastructure/database/books.json")
+
+    # Инициализация сценариев использования, передача репозитория в качестве зависимости
     add_book_use_case = AddBookUseCase(repo)
     change_status_use_case = ChangeBookStatusUseCase(repo)
     delete_book_use_case = RemoveBookUseCase(repo)
     search_book_use_case = SearchBookUseCase(repo)
 
+    # Определение словаря действий, где ключи - это выбор пользователя, а значения - функции, которые нужно выполнить
     actions = {
-        "1": lambda: add_book(add_book_use_case),
-        "2": lambda: delete_book(delete_book_use_case),
-        "3": lambda: search_books(search_book_use_case),
-        "4": lambda: display_books(repo.books),
-        "5": lambda: change_status(change_status_use_case),
+        "1": lambda: add_book(add_book_use_case),  # Добавление книги
+        "2": lambda: delete_book(delete_book_use_case),  # Удаление книги
+        "3": lambda: search_books(search_book_use_case),  # Поиск книги
+        "4": lambda: display_books(repo.books),  # Отображение всех книг
+        "5": lambda: change_status(change_status_use_case),  # Изменение статуса книги
     }
 
+    # Запуск главного меню и обработка пользовательского ввода
     display_menu(actions)
 
 
+# Точка входа в приложение
 if __name__ == "__main__":
     main()
